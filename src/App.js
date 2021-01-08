454ac9e3ec1a271f0467219bc0b29a78e4b3dc39
Andrei Zanoaga <andrei.zanoaga@adswizz.com>
2021-01-08 17:10:14 +0200

    changed app text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Andrei Zanoaga
         </a>
       </header>
     </div>
